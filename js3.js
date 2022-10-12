890ae783d0a0dbe47380c082a93e138804c268a1
selina rocha <selinarass94@gmail.com>
2022-10-12 18:28:57 -0500

    ejercicio js3

--- js3.js
+++ js3.js
@@ -1,13 +1,25 @@
 let horamed= Number(prompt("¿Cual fue la hora en la que tomo el medicamento la primera vez?"));
 let frecuencia= Number(prompt("¿Cada cuantas horas tiene que tomar el medicamento?"));
-tom=horamed+frecuencia;
+let dias= Number(prompt("¿Por cuantos dias tiene que tomar el medicamento?"));
+
+ite=(24*dias)/frecuencia;
 
 
-for (let i=1; i<=23;i++){
-    /*let codigo = "<marquee>" + i + "</marquee>";*/
-    n=frecuencia+tom;
-    let codigo = "la toma es a las: "+ tom;
-    document.write(codigo);
-    n+=i;
+for (i = 1; i<=ite;) {
+    if ( horamed>= 24) {
+        n= -1*(24-horamed) 
+        horamed= n
+        if (horamed = 0){
+            horamed= 0
+        }
+        document.write("La toma ", i, "es a las ", horamed, " horas " + "<br>");
+        
+        i= i+1
+        horamed=horamed+frecuencia
+    }
+    else {
+        document.write("La toma ", i, " es a las ", horamed, " horas " + "<br>");
+        i= i+1
+        horamed=horamed+frecuencia
+    }
 }
-
